3bde85acf369bcd32e25b2d36d5628d3617c46a9
Subhrajit Mukherjee <subhrajitmukh1234@gmail.com>
2023-03-05 00:45:14 +0530

    New

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import './App.css';
+import Header from "./Header";
 
 function App() {
   return (
-    <div className="app">
-      <h1>Hello,Msdian Lets </h1>
+   <div className="app">
       
+      <Header/>
     </div>
   );
 }
--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -3,12 +3,28 @@ import './Header.css'
 function Header() {
   return (
     <div className='header'>
-      <img className="header_logo" src="https://www.google.com/imgres?imgurl=https%3A%2F%2Frestaurantindia.s3.ap-south-1.amazonaws.com%2Fs3fs-public%2F2020-11%2Famazon.jpg&imgrefurl=https%3A%2F%2Frestaurant.indianretailer.com%2Fnews%2Famazon-starts-online-food-delivery-at-select-locations-in-delhi-bengaluru.n19530&tbnid=qtVJw2Pz5HtlnM&vet=12ahUKEwiw1by2tcD9AhUnJbcAHYImBnMQMygDegUIARDjAQ..i&docid=PIEA8Hv8nhvYlM&w=583&h=360&q=amazon&ved=2ahUKEwiw1by2tcD9AhUnJbcAHYImBnMQMygDegUIARDjAQ"/> 
+      <img className="header_logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"/> 
 
       <div className="header_search">
         <input className="header_searchInput" type="text"/>
       </div>
-      <div className="header_nav"></div>
+      <div className="header_nav">
+        <div className='header_option'>
+        <span className='header_optionLineOne'>Hello Guest</span>
+        <span className='header_optionLineTwo'>Sing In</span>
+        </div>
+
+        <div className='header_option'>
+        <span className='header_optionLineOne'>Returns</span>
+        <span className='header_optionLineTwo'>& Orders</span>
+        </div>
+
+        <div className='header_option'>
+        <span className='header_optionLineOne'>Your</span>
+        <span className='header_optionLineTwo'>Prime</span>
+        </div>
+
+      </div>
     </div>
   )
 }
