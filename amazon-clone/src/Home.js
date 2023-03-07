e6c7703aa2ef8e33a258da03dee5b127dff6afd0
Subhrajit Mukherjee <subhrajitmukh1234@gmail.com>
2023-03-07 23:23:02 +0530

    conti

--- amazon-clone/src/Home.js
+++ amazon-clone/src/Home.js
@@ -6,7 +6,7 @@ function Home() {
      <div className="home_container">
         <img className="home_image" src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg" alt=" "/>
         <div className="home__row">
-
+        
         </div>
         <div className="home__row">
 
