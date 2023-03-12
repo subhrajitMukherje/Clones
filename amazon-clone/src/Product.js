5354ea469d90d2d478346a879ed667fae1e81d68
Subhrajit <subhrajitmukh1234@gmail.com>
2023-03-12 10:34:08 +0530

    update in product

--- amazon-clone/src/Product.js
+++ amazon-clone/src/Product.js
@@ -15,6 +15,18 @@ function Product() {
       </div>
     <img src='https://www.haritbooks.com/wp-content/uploads/2022/10/ruskin-bond-the-blue-umbrella.jpg' alt=''/>
     <button>Add to Basket</button>
+    <div className='product_info'>
+        <p>The Blue Umbrella – Ruskin Bond</p>
+        <p className='product_price'>
+            <small>₹</small>
+            <strong>100</strong>
+        </p>
+        <div className='product_rating'>
+         <p>⭐</p>
+        </div>
+      </div>
+    <img src='' alt=''/>
+    <button>Add to Basket</button>
     </div>
   );
 }
